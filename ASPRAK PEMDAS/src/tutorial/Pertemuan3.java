ab5454fc41340016ca9bb5d216a284f475771ed4
itqanyrachman <89256655+itqanyrachman@users.noreply.github.com>
2022-10-24 10:22:00 +0700

    Add files via upload

---
+++ ASPRAK PEMDAS/src/tutorial/Pertemuan3.java
@@ -0,0 +1,37 @@
+package tutorial;
+
+public class Pertemuan3 {
+    
+    public static void main(String[] args) {
+        // int nilai = 100;
+        // float rataRata = 98.7825f;
+        // System.out.printf("Nilai saya adalah %d\n", nilai);
+        // System.out.printf("Rata-rata saya adalah %.2f\n",rataRata);
+        
+        // String nama = "Jamal";
+        // double nilai = 87.58;
+        // System.out.printf("Nilai SI-B %.3d\n", nilai);
+        
+        // Perpangkatan
+        // double desimal1 = Math.pow(27, 1/3);
+
+        // Akar 2
+        // double desimal2 = Math.sqrt(16);
+
+        // Akar 3
+        // double desimal3 = Math.cbrt(27);
+
+        // + untuk pertambahan
+        // - untuk pengurangan
+        // / untuk pembagian 
+        // * untuk perkalian
+        // % modulus : untuk sisa pembagian
+        
+        int a = 9 % 5;
+
+        System.out.println(a);
+
+    }
+    
+    
+}
